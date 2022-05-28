997548b6f240fc304de769d61feff9275283705a
Luca Gulak <lc.gk@hotmail.com>
2022-05-28 17:51:31 -0300

    Add clients logic

--- panel.js
+++ panel.js
@@ -1,3 +1,7 @@
+import { clientes } from "./data/clientes.js";
+
+let currentClient = 0;
+
 let forms = {};
 class PanelForm {
     constructor(formId, activatorId) {
@@ -52,4 +56,66 @@ window.onload = () => {
     new PanelForm("form-clients", "clients-activator");
     new PanelForm("form-products", "products-activator");
     new PanelForm("form-orders", "orders-activator");
+
+    // handle close forms
+    document.querySelectorAll(".close-form").forEach((btn) => {
+        btn.onclick = () => {
+            for (let form in forms) {
+                forms[form].hide();
+            }
+        }
+    })
+
+    // load first client
+    updateClient();
+
+    document.getElementById("form-clients__client-prev").onclick = handlePrevClient;
+    document.getElementById("form-clients__client-next").onclick = handleNextClient;
+    document.getElementById("form-clients__client-new").onclick = handleNewClient;
+    document.getElementById("form-clients__client-save").onclick = handleSaveClient;
+    
+}
+
+function updateClient() {
+    document.getElementById("form-clients__client-code").value = clientes[currentClient].codCliente;
+    document.getElementById("form-clients__client-name").value = clientes[currentClient].nomeCliente;
+    document.getElementById("form-clients__client-date").value = clientes[currentClient].dataCadCli;
+}
+
+function handlePrevClient() {
+    if (--currentClient < 0) {
+        alert("Você chegou ao início da lista de clientes!");
+        currentClient = 0;
+    }
+
+    updateClient();
+}
+
+function handleNextClient() {
+    if (++currentClient >= clientes.length) {
+        alert("Você chegou ao fim da lista de clientes!");
+        currentClient = clientes.length - 1;
+    }
+
+    updateClient();    
+}
+
+function handleNewClient() {
+    currentClient = clientes.length;
+
+    document.getElementById("form-clients__client-code").value = currentClient + 1;
+    document.getElementById("form-clients__client-name").value = "";
+    document.getElementById("form-clients__client-date").value = (new Date(Date.now())).toLocaleString("pt-BR").split(" ")[0];
+}
+
+function handleSaveClient() {
+    if (clientes[currentClient] == undefined) {
+        clientes[currentClient] = {};
+    }
+
+    clientes[currentClient].codCliente = parseInt(document.getElementById("form-clients__client-code").value);
+    clientes[currentClient].nomeCliente = document.getElementById("form-clients__client-name").value;
+    clientes[currentClient].dataCadCli = document.getElementById("form-clients__client-date").value; 
+
+    console.log(clientes);
 }
